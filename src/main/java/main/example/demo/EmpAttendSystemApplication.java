63a44c4a353cbe6b68faf2470eedc80f9a5d0b44
Mystic <thanhnhanjimmy@gmail.com>
2024-10-11 14:54:51 +0700

    Spring Boot Maven initialization

---
+++ src/main/java/main/example/demo/EmpAttendSystemApplication.java
@@ -0,0 +1,13 @@
+package main.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpAttendSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpAttendSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/main/example/demo/EmpAttendSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package main.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpAttendSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
