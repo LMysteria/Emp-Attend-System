048ef78a95201abde272527fa11bf5e9d7b3dd9a
Mystic <thanhnhanjimmy@gmail.com>
2024-10-16 09:31:58 +0700

    test return user object

--- src/main/java/app/attend_system/WebController.java
+++ src/main/java/app/attend_system/WebController.java
@@ -1,15 +1,24 @@
 package app.attend_system;
 
+import app.attend_system.Schemas.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebController {
+
+    User testUser = new User("Mystic","huh");
     
     @GetMapping("/hello")
-    public String getMethodName(@RequestParam(name="name", required = false, defaultValue = "World") String name) {
+    public String hello(@RequestParam(name="name", required = false, defaultValue = "World") String name) {
         return "Hello" + name;
     }
+
+    @GetMapping("/user")
+    public User getCurrentUser(){
+        return testUser;
+    }
+    
     
 }
